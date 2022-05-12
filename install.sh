e230fd1d9627a6a2bc56f94c05c787f88d60df5a
mayiyun <38709220+mayiyun@users.noreply.github.com>
2022-05-12 14:48:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -101,16 +101,16 @@ install_XrayR() {
             exit 1
         fi
         echo -e "检测到 XrayR 最新版本：${last_version}，开始安装"
-        wget -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux-64.zip https://down.mayiyun.vip/XrayR/XrayR-linux-64.zip
+        wget -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux-64.zip https://github.com/missuo/XrayR/releases/download/${last_version}/XrayR-linux-64.zip
         if [[ $? -ne 0 ]]; then
             echo -e "${red}下载 XrayR 失败，请确保你的服务器能够下载 Github 的文件${plain}"
             exit 1
         fi
     else
         last_version=$1
-        url="https://down.mayiyun.vip/XrayR/XrayR-linux-64.zip"
+        url="https://github.com/missuo/XrayR/releases/download/${last_version}/XrayR-linux-64.zip"
         echo -e "开始安装 XrayR v$1"
-        wget -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux-64.zip
+        wget -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux-64.zip ${url}
         if [[ $? -ne 0 ]]; then
             echo -e "${red}下载 XrayR v$1 失败，请确保此版本存在${plain}"
             exit 1
